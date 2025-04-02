90da4d8480613bced792de4597b702a0fced7704
Michael ZHANG <geek.michael@live.com>
2025-04-02 22:30:48 +1300

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,17 @@ module.exports = {
     title: `My 1st Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sitemap", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  plugins: [
+    // "gatsby-plugin-sitemap",
+    // "gatsby-plugin-mdx", {
+    // resolve: 'gatsby-source-filesystem',
+    // options: {
+    //   "name": "pages",
+    //   "path": "./src/pages/"
+    // },
+    // __key: "pages"
+    // },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
