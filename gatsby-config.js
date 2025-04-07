aa4e0c29aebf44545530bc9c89dd43ea3e4e0474
Michael ZHANG <geek.michael@live.com>
2025-04-07 21:05:15 +1200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,16 +7,15 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
-    // "gatsby-plugin-sitemap",
-    // "gatsby-plugin-mdx", {
-    // resolve: 'gatsby-source-filesystem',
-    // options: {
-    //   "name": "pages",
-    //   "path": "./src/pages/"
-    // },
-    // __key: "pages"
-    // },
+//    "gatsby-plugin-sitemap",
+//    "gatsby-plugin-mdx", 
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
+    "gatsby-plugin-sharp", {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
